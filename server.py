7ff7b93912e3060307a6bc15b436acb6809eb1ae
Matros777 <69822306+Matros777@users.noreply.github.com>
2026-07-08 02:21:54 +0300

    Update server.py

--- server.py
+++ server.py
@@ -63,9 +63,11 @@ PAYMENT_CONFIG = {
             "asset": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
             "payTo": "0x5b7efd37546d6BB02463339cEaDdD80997aC97B3",
             "maxTimeoutSeconds": 300,
-            "extra": {
+            "domain": {
                 "name": "USD Coin",
-                "version": "2"
+                "version": "2",
+                "chainId": 8453,
+                "verifyingContract": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
             }
         }
     ],
