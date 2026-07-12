cd46e753709dc7f90c654613ed89fb9a5a1968a4
Matros777 <69822306+Matros777@users.noreply.github.com>
2026-07-12 05:48:30 +0300

    Update server.py

--- server.py
+++ server.py
@@ -699,7 +699,10 @@ PAYMENT_CONFIG = {
                 "verifyingContract": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
             }
         }
-    ]
+    ],
+    "extensions": {
+        "bazaar": {}
+    }
 }
 
 
